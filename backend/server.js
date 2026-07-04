197b1d87d4a2e3542e49263dc02047ee8817a5e9
Abhishek Kumar <abhishekkumar63871@gmail.com>
2026-07-04 13:25:02 +0530

    Update SMTP configuration

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,9 @@ import expenseRouter from './routes/expenseRoute.js';
 import dashboardRouter from './routes/dashboardRoute.js';
 import contactRoutes from "./routes/contactRoutes.js";
 import dotenv from "dotenv";
+import net from "net";
+
+
 
 dotenv.config();
 const app = express();
@@ -27,6 +30,39 @@ app.use("/api/income", incomeRouter);
 app.use("/api/expense", expenseRouter);
 app.use("/api/dashboard" , dashboardRouter);
 app.use("/api", contactRoutes);
+
+app.get("/smtp-test", (req, res) => {
+  const socket = net.connect(587, "smtp-relay.brevo.com");
+
+  socket.setTimeout(10000);
+
+  socket.on("connect", () => {
+    socket.destroy();
+    res.json({
+      success: true,
+      message: "✅ Connected to Brevo SMTP",
+    });
+  });
+
+  socket.on("timeout", () => {
+    socket.destroy();
+    res.status(500).json({
+      success: false,
+      message: "❌ SMTP connection timed out",
+    });
+  });
+
+  socket.on("error", (err) => {
+    res.status(500).json({
+      success: false,
+      error: err.message,
+      code: err.code,
+    });
+  });
+});
+
+
+
 app.get('/', (req, res) => {
     res.send('API is running');
 });
